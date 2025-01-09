b8d987214c6b55edf237fd2d067a17c37c3c0742
FumCode <fumcode@gmail.com>
2025-01-09 14:02:38 +0200

    small http app

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var firstTask = "First Task"
+var secondTask = "Second Task"
+var therdTask = "Therd Task"
+var taskList = []string{firstTask, secondTask, therdTask}
 
 func main() {
-	fmt.Println("Hello Go")
+
+	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/tasks", showTasks)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Hello user"
+	fmt.Fprintln(writer, greeting)
+}
+
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+	for _, task := range taskList {
+		fmt.Fprintln(writer, task)
+	}
+
 }
